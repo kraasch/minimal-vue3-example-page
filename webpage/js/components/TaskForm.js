572bed00ca67e1257bbaab14f8f402e2b9a4415a
Alex Kraasch <alex@kraasch.eu>
2024-02-13 20:58:13 +0100

    Correct form.

--- webpage/js/components/TaskForm.js
+++ webpage/js/components/TaskForm.js
@@ -3,8 +3,8 @@ export default {
   template: `
       <form @submit.prevent='add_item'>
         <div class='border border-gray-600 text-black flex'>
-          <input v-model='new_task' placeholder='New task...' class=''/>
-          <button type='submit' class='bg-white border-l'>Add</button>
+          <input v-model='new_task' placeholder='New task...' class='w-10 flex-auto'/>
+          <button type='submit' class='bg-white border-l w-10'>Add</button>
         </div>
       </form>
   `,
@@ -14,8 +14,8 @@ export default {
       this.$emit('myadd', this.new_task);
       this.new_task = '';
     },
-  },
 
+  },
   data() {
     return {
       new_task: '',
